f310446352ba2840223e3db2c5728e47c348e91d
lidin888 <lidin888@github.com>
2025-12-05 08:25:27 +0800

    Fix CANPacket_t forward declaration to avoid conflicts

--- opendbc/safety/can.h
+++ opendbc/safety/can.h
@@ -5,17 +5,7 @@
 #define CANPACKET_DATA_SIZE_MAX 64U
 #define CAN_PACKET_VERSION 4
 
-// Forward declare CANPacket_t - actual definition in panda/board/can_declarations.h
-typedef struct {
-  unsigned char fd : 1;
-  unsigned char bus : 3;
-  unsigned char data_len_code : 4;
-  unsigned char rejected : 1;
-  unsigned char returned : 1;
-  unsigned char extended : 1;
-  unsigned int addr : 29;
-  unsigned char checksum;
-  unsigned char data[CANPACKET_DATA_SIZE_MAX];
-} CANPacket_t;
+// Use forward declaration to avoid conflicts with panda/board/can_declarations.h
+typedef struct CANPacket_s CANPacket_t;
 
 #define GET_LEN(msg) (dlc_to_len[(msg)->data_len_code])
